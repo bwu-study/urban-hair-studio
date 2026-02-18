e5a5a84090d54f51d71c98882209c1c0053569c5
isua0216 <isua966@gmail.com>
2026-02-18 12:45:53 +0900

    feat: add Nail-art page

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -2,9 +2,11 @@ import type { RouteObject } from 'react-router-dom'
 import Home from '../pages/Home'
 import Test from '../pages/Test'
 import Designer from '../pages/Designer'
+import Nail from '../pages/Nail'
 
 export const routes: RouteObject[] = [
   { path: '/', element: <Home /> },
   { path: '/test', element: <Test /> },
-  { path: '/designer', element: <Designer />}
+  { path: '/designer', element: <Designer /> },
+  { path: '/nail-art', element: <Nail />} 
 ]
