53f24d1f6ddee05993544a53a717814f034c7024
khy <hyeyeong.ijn@gmail.com>
2026-02-18 15:06:35 +0900

    feat: add location page

---
+++ src/pages/Location.tsx
@@ -0,0 +1,23 @@
+import {FloorInfoSection} from "@/components/location/FloorInfoSection.tsx";
+import {MapViewSection} from "@/components/location/MapViewSection.tsx";
+
+
+function Location() {
+    return (
+        <section className="bg-white mt-[30px]">
+            <div className="w-full">
+                <div className="flex justify-between items-start">
+                    <div className="w-[45%] px-8">
+                        <FloorInfoSection />
+                    </div>
+
+                    <div className="w-[50%]">
+                        <MapViewSection />
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+}
+
+export default Location;
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -2,9 +2,11 @@ import type { RouteObject } from 'react-router-dom'
 import Home from '../pages/Home'
 import Test from '../pages/Test'
 import Designer from '../pages/Designer'
+import Location from "@/pages/Location.tsx";
 
 export const routes: RouteObject[] = [
   { path: '/', element: <Home /> },
   { path: '/test', element: <Test /> },
-  { path: '/designer', element: <Designer />}
+  { path: '/designer', element: <Designer /> },
+  { path: '/location', element: <Location /> }
 ]
