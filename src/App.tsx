dcc876a722e9fb36cfdd1705e3a1c94f503e513b
matempong <107092881+MaTemPong@users.noreply.github.com>
2026-02-23 05:18:06 +0900

    fix: header, footer 재적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
-import { useRoutes } from 'react-router-dom'
-import { routes } from './routes/routes'
+import { useRoutes } from 'react-router-dom';
+import { routes } from './routes/routes';
+import { Header } from './components/common/Header';
+import { Footer } from './components/common/Footer';
 
 function App() {
-  const element = useRoutes(routes)
-  return element
+  const element = useRoutes(routes);
+  return (
+    <>
+      <Header />
+      {element}
+      <Footer />
+    </>
+  );
 }
 
-export default App
+export default App;
